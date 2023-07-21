77304228ade2b56c3fd6e45bedc82f2f7a7cae85
Eric <ericdgilbert2310@gmail.com>
2023-07-20 20:00:51 -0400

    created n days method

--- linear_regression.py
+++ linear_regression.py
@@ -9,3 +9,17 @@ class StockLinearRegression:
         linear_regression = LinearRegression().fit(X, y)
 
         return linear_regression.coef_
+
+    def creating_n_day_dfs(self, df, n):
+        necessary_cols = df[
+            ["date", "open", "high", "low", "close", "volume", "normalized_value"]
+        ]
+        n_day_values = []
+
+        for i in range(len(necessary_cols)):
+            try:
+                df_slice = necessary_cols[i : i + n]
+                n_day_values.append(df_slice)
+            except:
+                break
+        return n_day_values
