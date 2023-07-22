b5d8ebc1811c486b4693b899b554313ed90d2569
Eric <ericdgilbert2310@gmail.com>
2023-07-21 22:02:16 -0400

    del polygon_api_testing

--- linear_regression.py
+++ linear_regression.py
@@ -1,17 +1,17 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 
+
 class StockLinearRegression:
     def __init__(self) -> None:
         pass
 
-
-    def create_coef_data(self,df,days):
-        data={}
+    def create_coef_data(self, df, days):
+        data = {}
         for day in days:
             n_day_values = self.creating_n_day_dfs(df=df, n=day)
             n_day_coef_s = self.n_day_linear_regression(n_day_values=n_day_values)
-            col_name = f'{day}_day_linear_regression'
+            col_name = f"{day}_day_linear_regression"
             data[col_name] = n_day_coef_s
         coef_df = pd.DataFrame(data=data)
         return coef_df
@@ -19,20 +19,18 @@ class StockLinearRegression:
     def n_day_linear_regression(self, n_day_values):
         n_day_coef_s = []
         for df in n_day_values:
-            X = df.date.values.reshape(-1,1)
+            X = df.date.values.reshape(-1, 1)
             y = df.close.values
             coef_ = self.linear_regression_coefs(X=X, y=y)
             n_day_coef_s.append(coef_)
         return n_day_coef_s
-    
+
     def linear_regression_coefs(self, X, y):
         linear_regression = LinearRegression().fit(X, y)
         return linear_regression.coef_[0]
 
     def creating_n_day_dfs(self, df, n):
-        necessary_cols = df[
-            ["date", "open", "high", "low", "close", "volume", "normalized_value"]
-        ]
+        necessary_cols = df[["date", "close"]]
         n_day_values = []
         for i in range(len(necessary_cols)):
             try:
@@ -41,14 +39,3 @@ class StockLinearRegression:
             except:
                 break
         return n_day_values
-
-
-
-
-
-
-
-
-
-
-    
